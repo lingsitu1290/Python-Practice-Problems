ee118486f4e3e9dce4bfda5e5854d067fbd340cc
Ling Situ <lingsitu1290@gmail.com>
2016-11-21 18:11:51 -0800

    Added testing to OOP and list implementation

--- BT_implementation.py
+++ BT_implementation.py
@@ -7,6 +7,12 @@ class BinaryTree(object):
         self.rightChild = None
 
     def insertLeft(self, newNode):
+        """
+        >>> r = BinaryTree('a')
+        >>> r.insertLeft('c')
+        >>> print r.getLeftChild().getRootVal()
+        c
+        """
         if self.leftChild == None:
             self.leftChild = BinaryTree(newNode)
         else:
@@ -15,6 +21,12 @@ class BinaryTree(object):
             self.leftChild = t
 
     def insertRight(self, newNode):
+        """
+        >>> r = BinaryTree('a')
+        >>> r.insertRight('d')
+        >>> print r.getRightChild().getRootVal()
+        d
+        """
         if self.rightChild == None:
             self.rightChild = BinaryTree(newNode)
         else:
@@ -23,24 +35,60 @@ class BinaryTree(object):
             self.rightChild = t
 
     def getRightChild(self):
+        """
+        >>> r = BinaryTree('a')
+        >>> r.insertRight('b')
+        >>> print r.getRightChild().getRootVal()
+        b
+        """
         return self.rightChild
 
     def getLeftChild(self):
+        """
+        >>> r = BinaryTree('a')
+        >>> r.insertLeft('b')
+        >>> print r.getLeftChild().getRootVal()
+        b
+        """
+
         return self.leftChild 
 
     def setRootVal(self, obj):
+        """
+        >>> r = BinaryTree('a')
+        >>> r.insertLeft('b')
+        >>> r.setRootVal('c')
+        >>> print r.getRootVal()
+        c
+        """
         self.key = obj 
 
     def getRootVal(self):
+        """
+        >>> r = BinaryTree('a')
+        >>> r.getRootVal()
+        'a'
+        """
+
         return self.key
 
 
 # Tree Implementation using a list of a list
-def BinaryTree(r):
+def BinaryTreeL(r):
+    """
+    >>> r = BinaryTreeL(3)
+    >>> print r
+    [3, [], []]
+    """
     # construct the root node with two empty lists and children
     return [r,[],[]]
 
-def insertLeft(root, newBranch):
+def insertLeftL(root, newBranch):
+    """
+    >>> r = BinaryTreeL(3)
+    >>> insertLeftL(r, 4)
+    [3, [4, [], []], []]
+    """
     # the first index will be the left branch
     t = root.pop(1)
 
@@ -52,7 +100,12 @@ def insertLeft(root, newBranch):
 
     return root
 
-def insertRight(root, newBranch):
+def insertRightL(root, newBranch):
+    """
+    >>> r = BinaryTreeL(3)
+    >>> insertRightL(r, 4)
+    [3, [], [4, [], []]]
+    """
     t = root.pop(2)
 
     if len(t) > 1:
@@ -62,12 +115,36 @@ def insertRight(root, newBranch):
 
     return root
 
-def getRootVal(root):
+def getRootValL(root):
+    """
+    >>> r = BinaryTreeL(3)
+    >>> r = insertRightL(r, 4)
+    >>> getRootValL(r)
+    3
+    """
     return root[0]
 
-def getLeftChild(root):
+def getLeftChildL(root):
+    """
+    >>> r = BinaryTreeL(3)
+    >>> r = insertLeftL(r, 4)
+    >>> getLeftChildL(r)
+    [4, [], []]
+    """
     return root[1]
 
-def getRightChild(root):
+def getRightChildL(root):
+    """
+    >>> r = BinaryTreeL(3)
+    >>> r = insertLeftL(r, 4)
+    >>> right = getRightChildL(r)
+    >>> print right
+    []
+    """
     return root[2]
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. GREAT JOB!\n"
 
